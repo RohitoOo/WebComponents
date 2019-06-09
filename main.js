7b3a9323c164eb78f9a435ba3e9894559863d489
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-06-08 21:40:04 -0400

    Added Reusable Component

--- main.js
+++ main.js
@@ -1,27 +1,62 @@
 // import './todo-element.js';
 
 class NavBar extends HTMLElement {
-  constructor(){
+  constructor() {
     super()
-    this.attachShadow({mode: "open"})
+    this.attachShadow({ mode: "open" })
 
-    this.shadowRoot.innerHTML = `<h2>Hellooo! I'm a Web Component...</h2>`
+    let fetchTodos = async () => {
+      let json = await fetch("https://jsonplaceholder.typicode.com/todos")
+      let todoList = await json.json()
 
-    const main = document.querySelector("main")
+      todoList.map(todo => {
+        let a = document.createElement("a")
+        a.setAttribute("class", "collection-item")
 
-    // this.shadowRoot.appendChild()
-    // this.shadowRoot.appendChild(main) 
+        a.innerHTML = `<div>${todo.id}. ${
+          todo.title
+        } - <span class="badge">${todo.completed
+          .toString()
+          .toUpperCase()}</span></div>`
 
-  
+        a.className = "collection-item"
+        this.shadowRoot.appendChild(a)
+      })
+    }
+
+    fetchTodos()
+
+    const template = document.querySelector("template")
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
   }
-  connectedCallBack(){
+  connectedCallback() {
+    console.log("Connect To The DOM")
+  }
+  disconnectedCallback() {
     console.log("Connect To The DOM")
   }
 }
 
-window.customElements.define('nav-bar', NavBar)
+window.customElements.define("nav-bar", NavBar)
 
+// class Todo extends HTMLElement {
+//   constructor(){
+//     super()
+//     this.attachShadow({mode: "open"})
 
+//     this.shadowRoot.innerHTML = `<h2>Hellooo! I'm a Web Component...</h2>`
+
+//     const main = document.querySelector("main")
+
+//     // this.shadowRoot.appendChild()
+//     // this.shadowRoot.appendChild(main)
+
+//   }
+//   connectedCallBack(){
+//     console.log("Connect To The DOM")
+//   }
+// }
 
 // window.addEventListener("load", () => {
 //     fetchTodo()
@@ -30,7 +65,6 @@ window.customElements.define('nav-bar', NavBar)
 //   async function fetchTodo() {
 //     let json = await fetch("https://jsonplaceholder.typicode.com/todos")
 //     let todoList = await json.json()
-
 
 //     todoList.map(todo => {
 //       let el = document.createElement("todo-element")
@@ -45,5 +79,3 @@ window.customElements.define('nav-bar', NavBar)
 //       main.appendChild(el)
 //     })
 //   }
-
-
